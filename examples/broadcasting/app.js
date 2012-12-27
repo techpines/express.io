2dda3453529227f0e177192518d8e8377ce8363d
Brad Carleton <brad@techpines.com>
2012-12-27 13:20:35 -0600

    Still working on the examples.

--- examples/broadcasting/app.js
+++ examples/broadcasting/app.js
@@ -3,26 +3,8 @@ app = require('../../../express.io')()
 app.http().io()
 
 app.get('/', function(req, res) {
-    console.log(app.io)
-    app.io.broadcast('new-user')
-    app.io.broadcast.to('peeps').emit('cheese', {
-        dick: 'willies'
-    })
-    peeps = app.io.room('peeps')
-    peeps.broadcast('hey': you: 'rock')
+    app.io.broadcast('new visitor')
     res.sendfile(__dirname + '/client.html')
-})
-
-app.io.route('poop', function(req) {
-    console.log('*******************')
-    console.log(req.io.broadcast)
-    req.io.join('test')
-    request.io.room('
-    req.io.emit('hellow')
-    req.io.room('test').broadcast('cheese', {
-        dick: 'willies'
-    })
-    
 })
 
 app.listen(7076)
--- examples/rooms/app.js
+++ examples/rooms/app.js
@@ -1,16 +1,6 @@
 
-app = require('../../../express.io')()
+app = require('express.io')()
 app.http().io()
-
-app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/client.html')
-})
-
-app.get('/shout-out', function(req, res) {
-    app.io.room('users').broadcast('hey', {
-        guys: 'i love you'
-    })  
-})
 
 app.io.route('ready', function(req) {
     req.io.join('users')
@@ -19,7 +9,8 @@ app.io.route('ready', function(req) {
     })
 })
 
-app.io.route('hi-users', function(req) {
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/client.html')
 })
 
 app.listen(7076)
