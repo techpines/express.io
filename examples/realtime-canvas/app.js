54f7ce2e9ab01fb1bc9cefa44ed2273ab59392f3
Brad Carleton <brad@techpines.com>
2012-12-27 13:24:35 -0600

    Simplified the realtime canvas example.

--- examples/realtime-canvas/app.js
+++ examples/realtime-canvas/app.js
@@ -2,9 +2,6 @@
 express = require('express.io')
 app = express().http().io()
 
-// Static serve for drag and drop library.
-app.use(express.static(__dirname))
-
 // Broadcast all draw clicks.
 app.io.route('drawClick', function(req) {
     req.socket.broadcast.emit('draw', req.data)
